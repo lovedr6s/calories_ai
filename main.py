822695f4c96eb5599c629de7d72e3f5a3d46d9dd
lovedr6s <praense@icloud.com>
2025-06-17 19:02:39 +0300

    fix: save right style json files

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-import os
-from openai import OpenAI
-from dotenv import load_dotenv
-
-load_dotenv()
-
-client = OpenAI(api_key=os.getenv('ChatGPTApi'))
-prompt = os.getenv('prompt')
-
-
-def get_macros(products):
-    response = client.responses.create(
-        model= "gpt-4.1-nano",
-        input = prompt + products
-    )
-    return response.output_text
-
+from src import macros
 
 def main():
-    print(bot('150 грам риса курица жаренная в соевом соусе 100 гр'))
+    macros.save_to_json(macros.get_macros('банан 100гр и курица 100гр'))
+    #print(get_macros('150 грам риса курица жаренная в соевом соусе 100 гр'))
 
 if __name__ == '__main__':
     main()
