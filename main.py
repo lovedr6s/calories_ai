feb62148c530eab7d2ef7dec41f02ad45c4d92c7
lovedr6s <praense@icloud.com>
2025-06-17 18:12:10 +0300

    feat: add get_macros functio

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv('ChatGPTApi'))
+prompt = os.getenv('prompt')
+
+
+def get_macros(products):
+    response = client.responses.create(
+        model= "gpt-4.1-nano",
+        input = prompt + products
+    )
+    return response.output_text
+
+
 def main():
-    print("Hello from calories!")
+    print(bot('150 грам риса курица жаренная в соевом соусе 100 гр'))
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
