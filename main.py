3edf8c87024b4e4a3ff79b0c841eaf0a6166b002
lovedr6s <praense@icloud.com>
2025-06-18 09:50:23 +0300

    feat: started making webgui

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import requests
+import webview
+import threading
+import uvicorn
 from app.client import api_client
 
 
-
-def main():
-    text = input()
-    #api_client.save(text)
-    api_client.save(text)
+def run_gui():
+    uvicorn.run("app.webgui.gui:app", host="127.0.0.1", port=5000, reload=False)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    threading.Thread(target=run_gui, daemon=True).start()
+    webview.create_window("Lovedr6s", "http://127.0.0.1:5000", width=350, height=450, resizable=False)
+    webview.start()
