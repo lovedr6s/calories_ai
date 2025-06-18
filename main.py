390cb25f3f185380dfea2c095f3341016fab7870
lovedr6s <praense@icloud.com>
2025-06-18 09:25:28 +0300

    refactor: moved API to other folder. In future will be moved to other app fully

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import requests
 from app.client import api_client
 
 
+
 def main():
     text = input()
     #api_client.save(text)
-    print(api_client.load('2025-06-18'))
+    api_client.save(text)
 
 
 if __name__ == '__main__':
